ec85700b84f68cfb81cece69d21bc2d0638d7cec
Josue Arias <31876967+synthi@users.noreply.github.com>
2026-02-27 02:02:52 -0300

    Add initial elianne.lua for ELIANNE project
    Initial implementation of the Elianne project for Raspberry Pi 4 and Monome Norns, including grid and screen UI setup.

---
+++ elianne.lua
@@ -0,0 +1,73 @@
+-- elianne.lua
+-- Motor Principal - Proyecto ELIANNE (Grado Científico)
+-- Arquitectura: Raspberry Pi 4 / Monome Norns / Grid 128
+
+engine.name = 'Elianne'
+
+local G = include('lib/globals')
+local GridUI = include('lib/grid_ui')
+local ScreenUI = include('lib/screen_ui')
+-- local Matrix = include('lib/matrix') -- Se implementará en la Fase 2
+-- local Params = include('lib/params_setup') -- Se implementará en la Fase 2
+
+g = grid.connect()
+
+local grid_metro
+local screen_metro
+
+function init()
+    -- Inicializar base de datos de nodos
+    G.init_nodes()
+    
+    -- Inicializar caché del Grid
+    GridUI.init(G)
+    
+    -- Temporizador del Grid (30 fps para animaciones fluidas y respiración)
+    grid_metro = metro.init()
+    grid_metro.time = 1/30
+    grid_metro.event = function() 
+        GridUI.redraw(G, g) 
+    end
+    grid_metro:start()
+    
+    -- Temporizador de Pantalla (15 fps, protegido por screen_dirty)
+    screen_metro = metro.init()
+    screen_metro.time = 1/15
+    screen_metro.event = function() 
+        if G.screen_dirty then
+            redraw()
+            G.screen_dirty = false
+        end
+    end
+    screen_metro:start()
+    
+    print("ELIANNE: Sistema Forense Iniciado.")
+end
+
+function enc(n, d)
+    -- Enrutar encoders a la interfaz de pantalla
+    ScreenUI.enc(G, n, d)
+    G.screen_dirty = true
+end
+
+function key(n, z)
+    -- Enrutar teclas a la interfaz de pantalla
+    ScreenUI.key(G, n, z)
+    G.screen_dirty = true
+end
+
+g.key = function(x, y, z)
+    -- Enrutar eventos del Grid
+    GridUI.key(G, g, x, y, z)
+end
+
+function redraw()
+    screen.clear()
+    ScreenUI.draw(G)
+    screen.update()
+end
+
+function cleanup()
+    grid_metro:stop()
+    screen_metro:stop()
+end
